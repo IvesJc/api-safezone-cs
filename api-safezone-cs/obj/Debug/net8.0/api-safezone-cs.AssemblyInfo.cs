018e394ac75bf097a1c0655d44f3c061db9dfbbc
IvesJc-Evo <ives.chiba@evosystems.com.br>
2025-05-31 22:43:14 -0300

    chore config files

--- api-safezone-cs/obj/Debug/net8.0/api-safezone-cs.AssemblyInfo.cs
+++ api-safezone-cs/obj/Debug/net8.0/api-safezone-cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-safezone-cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d810b7292e08c75c1c7cd44f3e73a451035baa9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-safezone-cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-safezone-cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
