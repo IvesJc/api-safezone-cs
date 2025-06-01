e9e82b20b75466f3cf5742aaf4dc1e1ead76a2c9
IvesJc-Evo <ives.chiba@evosystems.com.br>
2025-05-31 22:46:20 -0300

    feat AppDbContext

---
+++ api-safezone-cs/Data/AppData/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using api_safezone_cs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_safezone_cs.Data.AppData;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Alerta> Alertas { get; set; }
+    public DbSet<Localizacao> Localizacaos { get; set; }
+    public DbSet<Ocorrencia> Ocorrencias { get; set; }
+    public DbSet<Vitima> Vitimas { get; set; }
+
+}
