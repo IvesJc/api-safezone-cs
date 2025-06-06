3809cfc1c827e74469dfa6bf445b1596780ae849
IvesJc-Evo <ives.chiba@evosystems.com.br>
2025-06-06 08:20:31 -0300

    chore

--- notification-service/Program.cs
+++
@@ -1,9 +0,0 @@
-using notification_service.Services;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Registra o BackgroundService
-builder.Services.AddHostedService<NotificationConsumer>();
-
-var app = builder.Build();
-app.Run();
